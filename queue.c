da9b64071d4397ace38129d311f1394db227b3a1
Collegei <wodianhua10@gmail.com>
2025-02-25 14:54:14 +0800

    Implement reverse and merge method
    Implement q_reverse, q_merge in queue.c, as well as related method.
    
    As commands in traces 3 involved reverse, and dm ops, which requires
    the implementation of methods mentioned aboved.
    
    However, face segmentation fault and infinite-loop-or-law-efficiency
    error. Probably because of q_merge.
    
    Change-Id: I9fac99740065aa70a3cd504627f34363adfc2de9

--- queue.c
+++ queue.c
@@ -103,7 +103,14 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 /* Return number of elements in queue */
 int q_size(struct list_head *head)
 {
-    return -1;
+    if (!head || head->next == head)
+        return 0;
+
+    int cnt = 0;
+    struct list_head *n;
+    list_for_each (n, head)
+        cnt++;
+    return cnt;
 }
 
 /* Delete the middle node in queue */
@@ -139,7 +146,14 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || head->next == head)
+        return;
+    struct list_head *n, *s;
+    list_for_each_safe (n, s, head)
+        list_move(n, head);
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
@@ -166,10 +180,92 @@ int q_descend(struct list_head *head)
     return 0;
 }
 
+#define bind(la, lb) ((la)->next = (lb), (lb)->prev = (la))
+#define contex_entry(n) list_entry((n), queue_contex_t, chain)
+
+/* Merge two sorted non-empty lists in to a double-circular-linked-list.
+ * Return it's head. */
+struct list_head *merge_two_list(struct list_head *la, struct list_head *lb)
+{
+    struct list_head *head = NULL, *tail = NULL;
+    struct list_head **back = &head;
+    while (la && lb) {
+        const char *va = list_entry(la, element_t, list)->value;
+        const char *vb = list_entry(lb, element_t, list)->value;
+        if (strcmp(va, vb) < 0) {
+            *back = la;
+            la->prev = tail;
+            tail = la;
+        } else {
+            *back = lb;
+            lb->prev = tail;
+            tail = lb;
+        }
+        back = &(*back)->next;
+    }
+    if (la)
+        *back = la;
+    else
+        *back = lb;
+
+    bind(tail, head);
+    return head;
+}
+
 /* Merge all the queues into one sorted queue, which is in ascending/descending
  * order */
 int q_merge(struct list_head *head, bool descend)
 {
     // https://leetcode.com/problems/merge-k-sorted-lists/
-    return 0;
+    if (head->next == head)
+        return 0;
+
+    while (head->next->next != head && contex_entry(head->next->next)->q) {
+        struct list_head *slow, *fast = head;
+        list_for_each (slow, head) {
+            if (fast->next == head || !contex_entry(fast->next)->q)
+                break;
+            if (fast->next->next == head ||
+                !contex_entry(fast->next->next)->q) {
+                struct list_head *h = contex_entry(fast->next)->q;
+                contex_entry(fast->next)->q = NULL;
+
+                contex_entry(slow)->q = h;
+                break;
+            }
+
+            struct list_head *ha = contex_entry(fast->next)->q;
+            contex_entry(fast->next)->q = NULL;
+
+            struct list_head *hb = contex_entry(fast->next->next)->q;
+            contex_entry(fast->next->next)->q = NULL;
+
+            if (!ha || ha->next == ha) {  // queue a is NULL or empty
+                if (ha)
+                    free(ha);
+                contex_entry(slow)->q = hb;
+                continue;
+            }
+
+            if (!hb || hb->next == hb) {  // queue b is NULL or empty
+                if (hb)
+                    free(hb);
+                contex_entry(slow)->q = ha;
+                continue;
+            }
+
+            ha->prev->next = NULL;
+            hb->prev->next = NULL;
+            struct list_head *lm = merge_two_list(ha->next, hb->next);
+
+            bind(lm->prev, ha);
+            bind(ha, lm);
+            free(hb);
+
+            contex_entry(slow)->q = ha;
+        }
+    }
+    if (descend)
+        q_reverse(contex_entry(head->next)->q);
+    return q_size(contex_entry(head->next)->q);
 }
